3ed9a0ef8d99b512e0bc0d26652c988a63a2d466
Leandro Miranda Fahur Machado <leandro@Leandros-MacBook-Pro-2.local>
2021-12-10 20:56:42 -0800

    update

--- arrays.php
+++ arrays.php
@@ -17,14 +17,38 @@
   // print_r($cars);  
   // var_dump($cars);
   
+
   // Associative arrays:
   $people = array('Leandro' => 35, 'Ocean' => 25, 'Aquila' => 24);
   // echo $people['Leandro'];
   // var_dump($people);
   $people['Guilherme'] = 25;
-  print_r($people);
+  // print_r($people);
   
 
+  // Multi-dimensional:
+  $cars = array(
+    array('Honda', 20, 10),
+    array('Toyota', 30, 20),
+    array('Ford', 25, 12),
+    array('Hyundai', 7, 7)
+  );
 
+  // count($cars) = 4
+  // count($cars[0]) = 3
+
+  for($i = 0; $i < count($cars); $i++) {
+    // for($j = 0; $j < count($cars[0]); $j++) {
+    //   if($j === 0)
+    //     echo 'Car: ' . $cars[$i][$j] . '<br>';
+    //   if($j === 1)
+    //     echo 'Inventory: ' . $cars[$i][$j] . '<br>';
+    //   if($j === 2)
+    //     echo 'Sold: ' . $cars[$i][$j] . '<br>';
+    // }           
+    echo 'Car: ' . $cars[$i][0] . ' | ';
+    echo 'Inventory: ' . $cars[$i][1] . ' | ';
+    echo 'Sold: ' . $cars[$i][2] . '<br><br>';
+  } 
 
 ?>
